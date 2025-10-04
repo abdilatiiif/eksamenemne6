c4e8465fb7f88460b5b5a80760b4376043dce4d0
Latif <latif_1995@live.no>
2025-10-04 14:13:39 +0200

    nav section start

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,74 @@
+'use client";';
+
+import Link from "next/link";
+import {
+  Leaf,
+  House,
+  HandHelping,
+  FlameKindling,
+  Calculator,
+} from "lucide-react";
+
+function Navigation() {
+  return (
+    <div className="bg-green-300 flex items-center justify-between px-4 p-2">
+      <Link
+        href="/"
+        className="flex items-center gap-2 text-2xl font-bold hover:text-green-600 transition"
+      >
+        <Leaf size={32} />
+        <span>Klimakampen</span>
+      </Link>
+
+      <nav className="hidden md:flex gap-3 ">
+        <Link href="/" className="hover:text-green-200 transition font-medium">
+          <button className="relative mt-5 p-2 text-md text-white overflow-hidden rounded-full group ">
+            <span className="relative z-10 text-white mix-blend-difference flex">
+              {" "}
+              <House size={20} /> Hjem
+            </span>
+            <span className="absolute left-0 top-0 h-full w-2/4 bg-white rounded-full transition-all duration-[850ms] ease-[cubic-bezier(0.68,-0.55,0.265,1.55)] group-hover:w-full"></span>
+          </button>
+        </Link>
+        <Link
+          href="/tiltak"
+          className="hover:text-green-200 transition font-medium flex"
+        >
+          <button className="relative mt-5 p-2 text-md text-white overflow-hidden rounded-full group">
+            <span className="relative z-10 text-white mix-blend-difference flex">
+              {" "}
+              <HandHelping size={20} /> Tiltak
+            </span>
+            <span className="absolute left-0 top-0 h-full w-2/4 bg-white rounded-full transition-all duration-[850ms] ease-[cubic-bezier(0.68,-0.55,0.265,1.55)] group-hover:w-full"></span>
+          </button>
+        </Link>
+        <Link
+          href="/verstinger"
+          className="hover:text-green-200 transition font-medium flex"
+        >
+          <button className="relative mt-5 p-2 text-md text-white overflow-hidden rounded-full group">
+            <span className="relative z-10 text-white mix-blend-difference flex">
+              {" "}
+              <FlameKindling size={20} /> Verstinger
+            </span>
+            <span className="absolute left-0 top-0 h-full w-2/6 bg-white rounded-full transition-all duration-[850ms] ease-[cubic-bezier(0.68,-0.55,0.265,1.55)] group-hover:w-full"></span>
+          </button>
+        </Link>
+        <Link
+          href="/kalkulator"
+          className="hover:text-green-200 transition font-medium flex"
+        >
+          <button className="relative mt-5 p-2 text-md text-white overflow-hidden rounded-full group">
+            <span className="relative z-10 text-white mix-blend-difference flex">
+              {" "}
+              <Calculator size={20} /> Klimakalkulator
+            </span>
+            <span className="absolute left-0 top-0 h-full w-1/4 bg-white rounded-full transition-all duration-[850ms] ease-[cubic-bezier(0.68,-0.55,0.265,1.55)] group-hover:w-full"></span>
+          </button>
+        </Link>
+      </nav>
+    </div>
+  );
+}
+
+export default Navigation;
